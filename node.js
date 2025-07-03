33860681abc51acadb3ff022e7c555667b1e6f11
Akila-19 <a.srinivasan036@mybvc.ca>
2025-07-03 17:49:51 -0600

    new change

--- node.js
+++ node.js
@@ -2,7 +2,7 @@ const http = require("http");
 const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.end("Hello from CI/CD pipeline!.... this is a new try... another change");
+  res.end("Welcome to the Mini CI/CD Project!\n");
 });
 server.listen(port, () => {
   console.log(`Server running at port ${port}`);

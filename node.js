3c58ba6b5f5ca3a0da5b22549159632b9b0cae48
Akila-19 <144281245+Akila-19@users.noreply.github.com>
2025-07-03 16:34:44 -0600

    Update node.js

--- node.js
+++ node.js
@@ -2,7 +2,7 @@ const http = require("http");
 const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.end("Hello from CI/CD pipeline!");
+  res.end("Hello from CI/CD pipeline!.... this is a new try...");
 });
 server.listen(port, () => {
   console.log(`Server running at port ${port}`);
